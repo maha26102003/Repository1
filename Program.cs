6c97ae64561f561dd168fea4b1b8aacce2c22acf
Mahalakshmi <bmahalakshmi775@gmail.com>
2024-12-21 13:22:23 +0530

    cv1

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gitproject
+{
+    internal class Program
+    {
+        int i, j;
+        static void Main(string[] args)
+        {
+            System.Diagnostics.Debug.WriteLine("Hello World");
+
+        }
+    }
+}
